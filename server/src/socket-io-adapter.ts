b9cb49e853711c9f7398c0ff2241e0da2776dba3
Arina Polienko <arinapolienko4@gmail.com>
2022-11-29 11:24:05 +0100

    add socket-io-adapter

---
+++ server/src/socket-io-adapter.ts
@@ -0,0 +1,30 @@
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+
+export class SocketIOAdapter extends IoAdapter {
+	private readonly logger = new Logger(SocketIOAdapter.name);
+	constructor(
+		private app: INestApplication,
+		private configService: ConfigService
+	) { super(app) };
+
+	createIOServer(port: number, options?: ServerOptions) {
+		const clientPort = parseInt(this.configService.get('CLIENT_PORT'));
+		const cors = {
+			origin: [
+				`http://localhost:${clientPort}`,
+				new RegExp(`/^http:\/\/192\.168\.1\.([1-9][1-9]\d):${clientPort}$/`).toString(),
+			],
+		};
+
+		this.logger.log(`Configuring SocketIO server with custom CORS option`, { cors });
+		const optionsWithCORS: ServerOptions = {
+			...options,
+			cors
+		};
+		return super.createIOServer(port, optionsWithCORS);
+	}
+}
+
